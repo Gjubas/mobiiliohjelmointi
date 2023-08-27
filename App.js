86ff9c9de4525a1e0d0e356897283ebd115102d7
Gjubas <oltsu5@hotmail.com>
2023-08-27 14:24:58 +0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button, Alert, TextInput, Image } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [text2, setText2] = useState('');
+
+  const buttonPressed = () => { 
+    Alert.alert('You typed: ' + text + ' and ' + text2); 
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image style={styles.image} source={{uri: 'https://www.sttinfo.fi/data/images/00304/be7db042-6b61-49f9-9bcd-7fd41b7bc35d.jpg'}} />
+      <TextInput style={styles.input} keyboardType='numeric' onChangeText={text => setText(text)} value={text}/>
+      <TextInput style={styles.input} onChangeText={text2 => setText2(text2)} value={text2}/>
+      <Button onPress={buttonPressed} title="Press me" />
+      <Text>This is text</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +27,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
+  image : {
+    width: 250,
+    height: 100
+  },
+  input : {
+    width:200  , 
+    borderColor: 'gray', 
+    borderWidth: 1
+  }
 });
